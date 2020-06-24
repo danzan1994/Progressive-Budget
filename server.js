87bcb473076c8a744ae718303c470d8fc20e0adc
Danzan Ubush <danzan1994@gmail.com>
2020-06-23 22:48:29 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
-
+// const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+// const herokuPORT = process.env.PORT;
+// if  (herokuPORT) {
+//   PORT = herokuPORT
+// }
+//  else {
+//    PORT = 3000
+//  }
 const app = express();
 
 app.use(logger("dev"));
